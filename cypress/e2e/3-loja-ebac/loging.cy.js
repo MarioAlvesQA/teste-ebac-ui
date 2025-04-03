21258a856c0b9708c5d5934bf5fefc053a59cfe3
Mario Alves <marioalves.qa@outlook.com>
2025-04-03 13:33:13 -0300

    Second commit after class 3 - Hooks (beforeEach, afterEach and method cy.screenshot() )

--- cypress/e2e/3-loja-ebac/loging.cy.js
+++ cypress/e2e/3-loja-ebac/loging.cy.js
@@ -2,8 +2,17 @@
 
 describe('Funcionalidade: Login', () => {
     // Bloco onde vai o codigo da automacao
-    it('Deve fazer com login com sucesso', () => {
+
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        
+    });
+
+    afterEach(() => {
+        cy.screenshot() // Faz uma captura de tela após cada teste, para verificar o resultado.
+    });
+
+    it('Deve fazer com login com sucesso', () => {
         cy.get('#username').type('marioteste@teste.com.br')
         cy.get('#password').type('Mario123..')
         cy.get('#rememberme').click()
@@ -13,4 +22,21 @@ describe('Funcionalidade: Login', () => {
         // .should('contain', ' deve ser seguido do texto que queremos validar ') 
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, marioteste (não é marioteste? Sair)') // metodo que confirma se o resultado de saída é o esperado 
     })
+    
+    // it.only ----> é utilizado para rodar apenas um teste, ele ignora os outros testes que estão dentro do mesmo describe. 
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido ', () => {
+        cy.get('#username').type('mario@teste.com.br')
+        cy.get('#password').type('Mario123..')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.') // Durante a validação pode conter o texto integral OU partes dele como "Endereço de e-mail desconhecido"
+        cy.get('.woocommerce-error > li').should('exist') // Verifica se o elemento existe na tela, se não existir o teste falha.
+    })
+
+    it('Deve exibir mensagem de erro ao inserir password inválido', () => {
+        cy.get('#username').type('marioteste@teste.com.br')
+        cy.get('#password').type('Errada@123..')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida para o e-mail marioteste@teste.com.br está incorreta. Perdeu a senha?')
+    });
+
 })
